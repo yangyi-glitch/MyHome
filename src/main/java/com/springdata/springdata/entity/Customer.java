7750be597cc47a37fe31b07522d6b1d3d84e603d
yangyi-glitch <1965997927@qq.com>
2024-07-29 18:42:36 +0800

    dwqd

--- src/main/java/com/springdata/springdata/entity/Customer.java
+++ src/main/java/com/springdata/springdata/entity/Customer.java
@@ -2,8 +2,12 @@ package com.springdata.springdata.entity;
 
 
 import lombok.Data;
+import org.springframework.expression.spel.ast.Literal;
 
 import javax.persistence.*;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 /**
  * Hibernate实体类
@@ -22,4 +26,24 @@ public class Customer {
 
     @Column(name = "cust_address")
     private String custAddress;
+
+    public static void main(String[] args) {
+        int[] a = {2,5,4,6,8,1,2,6,4,4};
+        int n = a.length;
+        while (n>0){
+            for (int i=0;i<n-1;i++){
+                if (a[i]>a[i+1]){
+                    int value = a[i];
+                    a[i] = a[i+1];
+                    a[i+1] = value;
+                }
+            }
+            n--;
+        }
+        System.out.print("[");
+        for(int e:a){
+            System.out.print(e+",");
+        }
+        System.out.println("]");
+    }
 }
