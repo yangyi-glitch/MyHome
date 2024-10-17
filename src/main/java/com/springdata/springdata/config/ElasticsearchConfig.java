52c572e70adef6550fc49ce4376636f7ac43d3b2
杨溢 <1965997927@qq.com>
2024-10-17 13:03:27 +0800

    111

--- src/main/java/com/springdata/springdata/config/ElasticsearchConfig.java
+++ src/main/java/com/springdata/springdata/config/ElasticsearchConfig.java
@@ -19,19 +19,4 @@ public class ElasticsearchConfig {
                 RestClient.builder(new HttpHost("192.168.218.131", 9200, "http")));
         return client;
     }
-
-    public static void main(String[] args) {
-        String x = "[{\"a\":1,\"b\":2},{\"a\":3,\"b\":4}]";
-        JSONArray jsonArray = new JSONArray(x);
-        for (int i=0;i<jsonArray.size();i++) {
-            Map<String,Integer> map = (Map<String, Integer>) jsonArray.get(i);
-            Integer a = map.get("a");
-            Integer b = map.get("b");
-            System.out.println(a+"+"+b+"="+(a+b));
-        }
-
-        String u = "我爱你";
-        String y = "我爱你";
-        System.out.println(u.equals(y));
-    }
 }
